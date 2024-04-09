2eb0afdb0015be611c175cc22075099bf3cc9901
apneduniya <apneduniya.dontdisturb@gmail.com>
2024-04-09 17:44:22 +0530

    update: Did some CSS chaneges in onboarding page

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -23,8 +23,8 @@ export default function OnboardingLayout() {
             <div className="h-[100dvh] w-full flex flex-col items-center justify-center py-5 px-4 gap-20">
                 <div className="flex flex-col items-center">
                     <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_kkflmtur.json" background="transparent" speed="1" style={{ height: 320, width: 320 }} loop autoplay></lottie-player>
-                    <h1 className="mt-10 text-left w-full text-2xl font-extrabold">Welcome to DataCrew!</h1>
-                    <p className="mt-2 text-justify text-gray-400">A data-to-earn platform emporing you to earn rewards by contributing data.</p>
+                    <h1 className="mt-10 text-left w-full pl-4 text-2xl font-extrabold">Welcome to DataCrew!</h1>
+                    <p className="mt-2 text-justify text-gray-400 pl-4 pr-4">A data-to-earn platform emporing you to earn rewards by contributing data.</p>
                 </div>
                 <WalletMultiButton />
             </div>
